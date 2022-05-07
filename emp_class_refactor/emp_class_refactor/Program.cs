7da18a443a17d68781946539b33378ab1db3a9e5
vrb9999 <vbiradar9999@gmail.com>
2022-05-07 15:35:14 +0530

    UC-7 Day-9 Class refactor

---
+++ emp_class_refactor/emp_class_refactor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace emp_class_refactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Total Employee Wage is : {class_refactor.computeEmpWage()}");
+        }
+    }
+}
---
+++ emp_class_refactor/emp_class_refactor/class_refactor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace emp_class_refactor
+{
+    class class_refactor
+    {
+        public const int IS_FULL_TIME = 2;
+        public const int IS_PART_TIME = 1;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
+
+        public static int computeEmpWage()
+        {
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+            }
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            return totalEmpWage;
+        }
+    }
+}
