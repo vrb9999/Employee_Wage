41a3adef8d4892ce4ed4a12a193d8a65ea4ae7e2
vrb9999 <vbiradar9999@gmail.com>
2022-05-07 15:52:56 +0530

    UC-9 Day-10 Total wage for each company

---
+++ TotalWageForEachCompany/TotalWageForEachCompany/Instance_variable.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TotalWageForEachCompany
+{
+    class Instance_variable
+    {
+		const int FULL_DAY_HOUR = 8;
+		const int PART_TIME_HOUR = 4;
+		const int EMP_FULL_TIME = 1;
+		const int EMP_PART_TIME = 2;
+		readonly string companyName;
+		readonly int wagePerHour;
+		readonly int maxWorkingDays;
+		readonly int maxWorkingHours;
+		int wagesPerMonth = 0;
+		public Instance_variable(string companyName, int wagePerHour, int maxWorkingDays, int maxWorkingHours)
+		{
+			this.companyName = companyName;
+			this.wagePerHour = wagePerHour;
+			this.maxWorkingDays = maxWorkingDays;
+			this.maxWorkingHours = maxWorkingHours;
+		}
+		public void ComputeWage()
+		{
+			int workingHours = 0;
+			int workingDays = 0;
+
+			Random rand = new Random();
+
+			while (workingHours < maxWorkingHours && workingDays < maxWorkingDays)
+			{
+				int attendance = rand.Next(0, 3);
+				int hoursWorked = 0;
+				switch (attendance)
+				{
+					case 1:
+						workingDays += 1;
+						hoursWorked = FULL_DAY_HOUR;
+						break;
+					case 2:
+						workingDays += 1;
+						hoursWorked = PART_TIME_HOUR;
+						break;
+					default:
+						break;
+				}
+				workingHours += hoursWorked;
+				wagesPerMonth += (wagePerHour * workingHours);
+			}
+		}
+		public override string ToString()
+		{
+			return $"Wages for the company {companyName} : {wagesPerMonth}";
+		}
+	}
+}
---
+++ TotalWageForEachCompany/TotalWageForEachCompany/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TotalWageForEachCompany
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Instance_variable comp1 = new Instance_variable("hcl", 100, 20, 100);
+            comp1.ComputeWage();
+            Console.WriteLine(comp1.ToString());
+            Instance_variable comp2 = new Instance_variable("hp", 150, 30, 150);
+            comp2.ComputeWage();
+            Console.WriteLine(comp2.ToString());
+        }
+    }
+}
